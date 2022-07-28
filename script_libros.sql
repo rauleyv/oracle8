c8441d56976f01eb2133eb4e62bfcef1a946aa32
rauleyv <rauleyv@protonmail.com>
2022-07-28 12:32:25 -0500

    second commit

---
+++ script_libros.sql
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2511
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c1\c1;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs22 \cf2 \CocoaLigature0 CREATE DATABASE libros;\
+USE libros;\
+\pard\pardeftab720\sl280\partightenfactor0
+
+\f1\fs24 \cf3 \expnd0\expndtw0\kerning0
+\CocoaLigature1 CREATE TABLE libros(titulo VARCHAR(50) NOT NULL,autor VARCHAR(30) NOT NULL,year INT NOT NULL,tema VARCHAR(30) NOT NULL,PRIMARY KEY(tema));\
+}
